c48a300440d6b7335952b997a54683d4e424eb89
OlegSadJktu <plyuha2004@mail.ru>
2021-12-08 15:26:13 +0600

    Mahaon task is done

--- news_blog/admin.py
+++ news_blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from news_blog.models import Article
+
+admin.site.register(Article)
+
 # Register your models here.
---
+++ news_blog/script.py
@@ -0,0 +1,5 @@
+#!../manage.py
+from news_blog.models import Article
+
+Article.objects.create(title='someTitle HEHE', text="from news_blog.models import ArticleArticle.objects.create(title=")
+Article.objects.create(title='awesome title', text="awesome title awesome title awesome title awesome title")
